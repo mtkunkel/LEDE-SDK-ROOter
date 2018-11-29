53246393d031930daa1e8d0c407596cdf21ef867
Matt Kunkel <pilotmattk@gmail.com>
2018-11-28 23:12:03 -0600

    LEDE/OpenWRT to 17.01.6 Add Stubby, GetDNS, cacerts for DNS over TLS patch rooter packages for EM7565

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Hacked together & hard-coded example to demonstrate how to build a ROOter (from ofmodemsandmen.com) that maintains LEDE kernel compatibility.
 
-Example target is a WRT3200ACM.   Building from the LEDE development snapshot, any LEDE >= 17.01.2 should work.
+Example target is a WRT3200ACM.   Building from the LEDE development snapshot, any LEDE >= 17.01.6 should work.
 
 .config file is pre-generated for the above example, run `make menuconfig` to manually generate .config. 
 In the menu:
@@ -11,3 +11,8 @@ Must __DE-select__ the following in __Global Build Settings__ prior to ./scripts
 * all kernel module packages by default
 * all userspace packages by default
 Select relevant rooter packages with [M] for module (ext-rooter8).
+
+
+Added rooter patches for Sierra Wireless EM7565
+
+Stubby pulled in from 18 and snapshot packages for DNS over TLS
--- rooter_sdk_build.sh
+++ rooter_sdk_build.sh
@@ -1,25 +1,48 @@
+##########################################################################
+#    Copyright 2017, 2017 Matthew Kunkel
+##########################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #!/bin/bash
 
-wget -N --content-disposition https://downloads.lede-project.org/snapshots/targets/mvebu/generic/lede-sdk-mvebu_gcc-5.5.0_musl_eabi.Linux-x86_64.tar.xz
+OPENWRT_VERSION=17.01.6
 
-wget -N --content-disposition https://downloads.lede-project.org/snapshots/targets/mvebu/generic/lede-imagebuilder-mvebu.Linux-x86_64.tar.xz
+wget -N --content-disposition https://downloads.openwrt.org/releases/$OPENWRT_VERSION/targets/mvebu/generic/lede-sdk-$OPENWRT_VERSION-mvebu_gcc-5.4.0_musl-1.1.16_eabi.Linux-x86_64.tar.xz
 
-wget -N --content-disposition https://ofmodemsandmen.com/download/rooter.zip
+wget -N --content-disposition https://downloads.openwrt.org/releases/$OPENWRT_VERSION/targets/mvebu/generic/lede-imagebuilder-$OPENWRT_VERSION-mvebu.Linux-x86_64.tar.xz
 
-unzip rooter.zip
-tar -xf lede-sdk*.tar.xz
-tar -xf lede-imagebuilder*.tar.xz
+wget -N --content-disposition  https://ofmodemsandmen.com/download/GoldenOrb2017-12-15-8meg.zip
+
+unzip GoldenOrb2017-12-15-8meg.zip
+patch -p0 <rooter.patch
+
+tar -xf lede-sdk-$OPENWRT_VERSION*.tar.xz
+tar -xf lede-imagebuilder-$OPENWRT_VERSION*.tar.xz
 rm *.tar.xz *.zip
-mv ./rooter/* lede-sdk*/package
+mv ./rooter/* lede-sdk-$OPENWRT_VERSION*/package
 #If manually executing for different hardware/LEDE version, run make menuconfig and select options per README.
 #.config could be pre-generated for automated builds
 cp .config lede-sdk*
-cd lede-sdk*
+#exit
+cd lede-sdk-$OPENWRT_VERSION*
 scripts/feeds update -a
-scripts/feeds install libubox wireless-regdb
+scripts/feeds install libubox 
 make
-find . -name "*.ipk" -exec mv {} ../lede-imagebuilder*/packages \;
-cd ../lede-imagebuilder*
-DEFAULT_PACKAGES=$(wget -qO - https://downloads.lede-project.org/snapshots/targets/mvebu/generic/lede-mvebu.manifest | sed  's/ - .*/ /' | sed  's/dnsmasq//' | tr '\n' ' ')
+find . -name "*.ipk" -exec mv {} ../lede-imagebuilder-$OPENWRT_VERSION*/packages \;
+cd ../lede-imagebuilder-$OPENWRT_VERSION*
+wget -N --content-disposition -P ./packages/ https://downloads.openwrt.org/snapshots/packages/arm_cortex-a9_vfpv3/packages/stubby_0.2.3-3_arm_cortex-a9_vfpv3.ipk
+wget -N --content-disposition -P ./packages/ https://downloads.openwrt.org/releases/18.06.1/packages/arm_cortex-a9_vfpv3/packages/getdns_1.4.2-1_arm_cortex-a9_vfpv3.ipk
+DEFAULT_PACKAGES=$(wget -qO - https://downloads.openwrt.org/releases/$OPENWRT_VERSION/targets/mvebu/generic/lede-$OPENWRT_VERSION-mvebu.manifest | sed  's/ - .*/ /' | sed  's/dnsmasq//' | tr '\n' ' ')
 
-make image PROFILE="linksys-wrt3200acm" PACKAGES="$DEFAULT_PACKAGES luci-ssl ext-rooter8 rmbim rqmi luci-proto-mbim -dnsmasq dnsmasq-full dnscrypt-proxy luci-app-openvpn luci-theme-darkmatter luci-theme-rooter"
+make image PROFILE="linksys-wrt3200acm" PACKAGES="$DEFAULT_PACKAGES luci-ssl ext-rooter8 rmbim rqmi luci-proto-mbim -dnsmasq dnsmasq-full getdns stubby ca-certificates luci-app-openvpn luci-theme-darkmatter luci-theme-rooter bind-dig"
